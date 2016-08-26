86f9e38a6b89ea53d87bf758c0a9740a6b383d06
robin <robin@makichan.moe>
2016-08-26 20:33:17 +0900

    sql 모듈 추가

--- dependencies.js
+++ dependencies.js
@@ -4,6 +4,22 @@ var func = {};
 func.$body = (req, next) => {
 	next(null, req.body);
 };
-
+func.$sql = (req, next) => {
+	next(null, (query, data, cb) => {
+		if(!cb) {
+			cb = data;
+			data = [];
+		}
+		var sql = db.query(query, data, (err, result) => {
+			if(!err) {
+				cb(null, result);
+			}
+			else {
+				console.error('SQL ERROR: ' + sql.query);
+				cb(err);
+			}
+		});
+	});
+};
 
 module.exports = func;
